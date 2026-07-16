85df0389c7e912f510fe58bf3befa1140cce2ce1
Georgios Konstantopoulos <me@gakonst.com>
2026-07-16 15:12:29 -0700

    Pin upstream XcodeGen release in Nix shell

--- flake.nix
+++ flake.nix
@@ -16,6 +16,23 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          xcodegen = pkgs.stdenvNoCC.mkDerivation {
+            pname = "xcodegen";
+            version = "2.45.3";
+            src = pkgs.fetchurl {
+              url = "https://github.com/yonaskolb/XcodeGen/releases/download/2.45.3/xcodegen.artifactbundle.zip";
+              hash = "sha256-ajy4QYPH/Ijr4Hlq9qhLvwewBbzcYequ8TqKZh0Gdbg=";
+            };
+            dontUnpack = true;
+            installPhase = ''
+              runHook preInstall
+              ${pkgs.unzip}/bin/unzip "$src" -d unpacked
+              install -Dm755 \
+                unpacked/xcodegen.artifactbundle/xcodegen-2.45.3-macosx/bin/xcodegen \
+                "$out/bin/xcodegen"
+              runHook postInstall
+            '';
+          };
         in
         {
           default = pkgs.mkShellNoCC {
