0b3b8238cb8eb4abff4928b2964d0083399b201b
Georgios Konstantopoulos <me@gakonst.com>
2026-07-16 15:08:26 -0700

    Support Nix complgen completion generation

--- build-shell-completion.sh
+++ build-shell-completion.sh
@@ -8,16 +8,16 @@ rm -rf .shell-completion && mkdir -p \
     .shell-completion/bash
 
 if /usr/bin/which complgen &> /dev/null; then
-    complgen_bin=complgen
+    complgen ./grammar/commands-bnf-grammar.txt --zsh .shell-completion/zsh/_aerospace
+    complgen ./grammar/commands-bnf-grammar.txt --fish .shell-completion/fish/aerospace.fish
+    complgen ./grammar/commands-bnf-grammar.txt --bash .shell-completion/bash/aerospace
 else
     ./script/install-dep.sh --complgen
-    complgen_bin=./.deps/cargo-root/bin/complgen
+    ./.deps/cargo-root/bin/complgen aot ./grammar/commands-bnf-grammar.txt \
+        --zsh-script .shell-completion/zsh/_aerospace \
+        --fish-script .shell-completion/fish/aerospace.fish \
+        --bash-script .shell-completion/bash/aerospace
 fi
-
-$complgen_bin aot ./grammar/commands-bnf-grammar.txt \
-    --zsh-script .shell-completion/zsh/_aerospace \
-    --fish-script .shell-completion/fish/aerospace.fish \
-    --bash-script .shell-completion/bash/aerospace
 
 # Check basic syntax
 zsh -c 'autoload -Uz compinit; compinit; source ./.shell-completion/zsh/_aerospace'
